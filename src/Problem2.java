eb12f5d5b91193b7b65fca4bee4754973dfee2a3
BT_4N6_11 <alexanderm474@nycstudents.net>
2026-09-23 23:07:23 -0400

    issue with player 2's name and name2

--- src/Problem2.java
+++ src/Problem2.java
@@ -2,7 +2,7 @@ import  java.util.Scanner;
 public class Problem2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Player 1, enter your name: ");
+        System.out.print("Player 1, enter your name:");
         String name = scan.nextLine();
         System.out.print("Enter your number: ");
         double number = scan.nextDouble();
