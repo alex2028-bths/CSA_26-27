6a797871557fabf5c48fe9c9575eb8bf2f86ddb8
BT_4N6_11 <alexanderm474@nycstudents.net>
2026-09-23 23:04:39 -0400

    issue with player 2's name and name2

--- src/Problem2.java
+++ src/Problem2.java
@@ -2,7 +2,21 @@ import  java.util.Scanner;
 public class Problem2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Player 1, enter your name: ");
+        System.out.print("Player 1, enter your name: ");
         String name = scan.nextLine();
+        System.out.print("Enter your number: ");
+        double number = scan.nextDouble();
+        System.out.print("Player 2, enter your name: ");
+        String name2 = scan.nextLine();
+        System.out.print("Enter your number: ");
+        double number2 = scan.nextDouble();
+        if (number > number2 ){
+            System.out.print(name+"'s number is bigger!");
+        } else if (number < number2){
+            System.out.print(name2+"'s number is bigger!");
+        } else  {
+            System.out.print("The numbers are equal!");
+        }
+
     }
 }
