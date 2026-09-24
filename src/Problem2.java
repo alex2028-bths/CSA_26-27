0567b82ee090464fc826aeca7e8e06aa6dcdacd7
BT_4N6_11 <alexanderm474@nycstudents.net>
2026-09-24 15:03:46 -0400

    fixed issue

--- src/Problem2.java
+++ src/Problem2.java
@@ -6,17 +6,18 @@ public class Problem2 {
         String name = scan.nextLine();
         System.out.print("Enter your number: ");
         double number = scan.nextDouble();
+        scan.nextLine();
         System.out.print("Player 2, enter your name: ");
         String name2 = scan.nextLine();
         System.out.print("Enter your number: ");
         double number2 = scan.nextDouble();
-        if (number > number2 ){
-            System.out.print(name+"'s number is bigger!");
-        } else if (number < number2){
-            System.out.print(name2+"'s number is bigger!");
-        } else  {
+        if (number > number2) {
+            System.out.print(name + "'s number is bigger!");
+        } else if (number < number2) {
+            System.out.print(name2 + "'s number is bigger!");
+        } else {
             System.out.print("The numbers are equal!");
-        }
 
+        }
     }
 }
