7a4c07304f6d66c1f424ee9ff3ce6499a6bf27a8
BT_4N6_11 <alexanderm474@nycstudents.net>
2026-09-23 22:41:48 -0400

    added Problem1

---
+++ src/Problem1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Welcome " + name + "!");
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Please enter the first integer: ");
+        int firstInt = input2.nextInt();
+        Scanner input3 = new Scanner(System.in);
+        System.out.println("Please enter the second integer: ");
+        int secondInt = input3.nextInt();
+        Scanner input4 = new Scanner(System.in);
+        System.out.println("Please enter the third integer: ");
+        int thirdInt = input4.nextInt();
+        double average = (firstInt + secondInt + thirdInt) / 3.0;
+        System.out.println("The average of " + firstInt + ", " + secondInt + ", and " + thirdInt + " is " + average);
+    }
+}
